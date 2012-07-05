8396a0ce1486f383133076c27d7754c46807b100
gnal <qwertypianox@gmail.com>
2012-07-04 22:23:35 -0400

    gg

--- Entity/MenuManager.php
+++ Entity/MenuManager.php
@@ -6,5 +6,19 @@ use Msi\Bundle\AdminBundle\Entity\ModelManager;
 
 class MenuManager extends ModelManager
 {
+    public function findRootById($id)
+    {
+        $qb = $this->findBy(array('c.enabled' => true, 'a.enabled' => true, 'a.id' => $id), array('a.children' => 'c', 'c.translations' => 'ct', 'c.page' => 'p', 'p.translations' => 'pt'), array());
 
+        $orX = $qb->expr()->orX();
+
+        $orX->add($qb->expr()->eq('pt.locale', ':ptlocale'));
+        $qb->setParameter('ptlocale', $this->session->getLocale());
+
+        $orX->add($qb->expr()->isNull('c.page'));
+
+        $qb->andWhere($orX);
+
+        return $qb->getQuery()->getSingleResult();
+    }
 }
