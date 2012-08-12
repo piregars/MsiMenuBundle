37b1ca6b974189d794f54669713283261a8adffa
gnal <qwertypianox@gmail.com>
2012-08-11 23:53:15 -0400

    upgrading to 2.1

--- Form/Type/MenuNodeTranslationType.php
+++ Form/Type/MenuNodeTranslationType.php
@@ -3,16 +3,24 @@
 namespace Msi\Bundle\MenuBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class MenuNodeTranslationType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name')
             ->add('route')
         ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Msi\Bundle\MenuBundle\Entity\MenuTranslation',
+        ));
     }
 
     public function getName()
--- Form/Type/MenuRootTranslationType.php
+++ Form/Type/MenuRootTranslationType.php
@@ -3,15 +3,23 @@
 namespace Msi\Bundle\MenuBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class MenuRootTranslationType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name')
         ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Msi\Bundle\MenuBundle\Entity\MenuTranslation',
+        ));
     }
 
     public function getName()
